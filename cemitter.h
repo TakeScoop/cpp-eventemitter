967db3608de2603a46586e3ad9ed0455aef46f66
Jamie Beverly <jamie@takescoop.com>
2017-06-22 10:22:29 -0700

    Fix const correctness on C bindings
    didn't mark the void* as const void*
    also fixed the lack of _cplusplus guards around 'extern "C"'

--- cemitter.h
+++ cemitter.h
@@ -2,9 +2,13 @@
 #ifndef _GLPK_EVENTEMITTER_CEMITER_H
 #define _GLPK_EVENTEMITTER_CEMITER_H
 
+#ifdef __cplusplus
 extern "C" {
+#endif
 typedef void (*eventemitter_fn)(const char*, const char*);
-typedef void (*eventemitter_fn_r)(void* sender, const char*, const char*);
+typedef void (*eventemitter_fn_r)(const void* sender, const char*, const char*);
+#ifdef __cplusplus
 };
+#endif
 
 #endif
